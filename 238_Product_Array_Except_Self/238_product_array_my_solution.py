5c3fc7bfb01363d5a0077ece166bf35f83770ed3
Gabriel <gabrielassisdepaula@gmail.com>
2024-10-12 12:50:24 -0300

    Add 304 readme and 238 first solution

---
+++ 238_Product_Array_Except_Self/238_product_array_my_solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        prefix_product = []
+        postfix_product = [None]*len(nums)
+        
+        total_prefix = 1
+        for num in nums:
+            total_prefix *= num
+            prefix_product.append(total_prefix)
+        
+        total_postfix = 1
+        for idx in range(len(nums)-1, -1, -1):
+            total_postfix *= nums[idx]
+            postfix_product[idx] = total_postfix
+
+        output = []
+        for idx in range(len(nums)):
+            prefix = prefix_product[idx-1] if idx > 0 else 1
+            postfix = postfix_product[idx+1] if idx < len(nums)-1 else 1
+            output.append(prefix*postfix)
+
+
+        return output
+
+sol = Solution()
+nums = [1,2,3,4]
+print(sol.productExceptSelf(nums))
---
+++ 304_Range_Sum_Query_2D/README.md
@@ -0,0 +1,16 @@
+
+# 304. Range Sum Query 2D - Immutable
+
+### Range Sum 1
+
+After understanding how 2D Prefix Sums are calculated I coded the first solution with the math help from Profound Academy - 2D Prefix Sum and Sumatrix Sum Queries video.
+
+It has a lot of comments to help me understand in case I forget something.
+
+### Range Sum 2
+
+After the first solution I went to check on NeetCode's solution. I liked the prefix sum calculation a bit better. The sum function was pretty much the same.
+
+### Range Sum 3
+
+Merged both solutions and made it with less code and thus less readabilty. Also removed most of the coments.
